e3b84af741c7b2b2fe50d479bc8902556d6553f4
Pauline <eaapausa@students.eaaa.dk>
2020-10-14 13:29:44 +0200

    onsdag

--- taelskridt/js/js.js
+++ taelskridt/js/js.js
@@ -8,12 +8,45 @@ console.log('file: js/js.js loaded');
 $(document).ready(function () {
 
 	console.log("jQuery 3.5.1 running. Alert level");
-
+    $('#betaling').hide();
+    $('#bekraeftelse').hide();
+    
     
     $('#bar').click(function() {
          $('#navul').toggle("slow");
     });
-
+    
+    $('#page1').click(function(){
+       $('#oplysninger').show(); 
+        $('#betaling').hide(); 
+        $('#bekraeftelse').hide();
+    });
+    
+    
+    $('#page2').click(function(){
+       $('#betaling').show(); 
+        $('#bekraeftelse').hide();
+        $('#oplysninger').hide(); 
+    });
+    
+     $('#page3').click(function(){
+       $('#betaling').hide(); 
+        $('#bekraeftelse').show(); 
+         $('#oplysninger').hide();
+    });
+    
+    
+    $('.videre').click(function(){
+       $('#betaling').show(); 
+        $('#oplysninger').hide();
+    });
+    
+    
+    $('.bekraeft').click(function(){
+       $('#bekraeftelse').show(); 
+        $('#oplysninger').hide();
+        $('#betaling').hide();
+    });
     
    });
     
