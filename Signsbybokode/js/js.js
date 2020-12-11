e4c28bb0b02bd65f5717c65453f85441916d7d9d
paulineandersen <60916217+paulineandersen@users.noreply.github.com>
2020-12-11 14:01:02 +0100

    Add files via upload

--- Signsbybokode/js/js.js
+++ Signsbybokode/js/js.js
@@ -6,91 +6,195 @@ console.log('Hej det virker!');
 
 
 //document.ready function tjekker om DOM'en er loaded
-$(document).ready(function() {
-            
-console.log('Hej fra js/js.js: nu med jQuery!');
-    
+$(document).ready(function () {
+
+    console.log('Hej fra js/js.js: nu med jQuery!');
+
     // overskrift animation
-    $(window).scroll(function(){
-    $("#scroll").css("opacity", 1 - $(window).scrollTop() / 650);
+    $(window).scroll(function () {
+        $(".scroll").css("opacity", 1 - $(window).scrollTop() / 650);
     });
-    
-    $(window).scroll(function(){
-    $(".forsidevideo").css("opacity", 1 - $(window).scrollTop() / 5000);
+
+
+
+    $(window).scroll(function () {
+        $(".forsidevideo").css("opacity", 1 - $(window).scrollTop() / 5000);
     });
-    
+
     // Kontakt knappen kan foldes ud
-    $('#klik').hide(); 
-    
-    $('#takt').click(function() {
-        
-    $('#klik').toggle(10);
+    $('#klik').hide();
+
+    $('#takt').click(function () {
+
+        $('#klik').toggle(10);
     });
-    
-    $('.kontakt').click(function() {
+
+    $('.kontakt').click(function () {
         $('#klik').toggle(10);
     });
 
 
-/* arrow */
+    /* arrow */
 
-$('.pilflex').hover(function () {
-    $('.right').animate({left: '20px'});
-}, function(){
-    $('.right').animate({left: '0px'})
-    }); 
-    
-    
+    $('.pilflex').hover(function () {
+        $('.right').animate({
+            left: '20px'
+        });
+    }, function () {
+        $('.right').animate({
+            left: '0px'
+        })
+    });
 
-    
-
-/** scroll effekt
-    let Synlig = 0;
-$(window).scroll(function(){
-    console.log('der scrolles!');
-    $('.teaser1').css("opacity", Synlig + 1); 
-}); **/
-    
     /**
-    
-    var top_of_element = $("#hero").offset().top;
-    var bottom_of_element = $("#hero").offset().top + $("#hero").outerHeight();
-    var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
-    var top_of_screen = $(window).scrollTop();
+        let topTest, topPosition; // bruges til at gemme data om elementerne
 
-    if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
-        // the element is visible, do something
-        console.log('I am visible now');
-        $('#hero').addClass('brugAnimation');
+        // ved scrolle testes om et elements topposition svarer til scroll
+        $(window).scroll(function () {
+
+            topTest = $(".teaser1").offset().top;
+            topPosition = $(window).scrollTop();
+
+            console.log('Scroll - nu er scrollpositonen : ' + topPosition);
+
+            if ((topPosition) >= topTest) {
+                console.log('YABBADABBADOOOOOO'); // giver lidt luft i toppen ;-)
+                $('.teaser1').css('filter', 'opacity(1)');
+                // gør noget med dine elementer her ...
+                // $('#dinIdA').css(etc.)
+            } else {
+                $('.teaser1').css('filter', 'opacity(0)');
+            }
+
+        });
+
+        **/
+    
+    let topTest, topPosition; 
+
+
+    $(window).scroll(function () {
+
+        var top_of_element = $(".billede1").offset().top;
+        var bottom_of_element = $(".billede1").offset().top + $(".billede1").outerHeight();
+        var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+        var top_of_screen = $(window).scrollTop();
+
+        if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
+            // the element is visible, do something
+            console.log('I am visible now');
+            $('.billede1').addClass('brugAnimation');
+           //kører animationen fra css
         } else {
-        // the element is not visible, do something else
-        console.log('I know noothing!')
-        // ... hide again ...
-    }
-}); */
+            // the element is not visible, do something else
+            console.log('I am invisible now ... fading down.')
+            $('.billede1').css('filter', 'opacity(1)');
+            // ... hide again ...
+        }
+    });
 
+
+    $(window).scroll(function () {
+
+        var top_of_element = $(".billede2").offset().top;
+        var bottom_of_element = $(".billede2").offset().top + $(".billede2").outerHeight();
+        var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+        var top_of_screen = $(window).scrollTop();
+
+        if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
+            // the element is visible, do something
+            console.log('I am visible now');
+            $('.billede2').addClass('brugAnimation');
+           //kører animationen fra css
+        } else {
+            // the element is not visible, do something else
+            console.log('I am invisible now ... fading down.')
+            $('.billede2').css('filter', 'opacity(1)');
+            // ... hide again ...
+        }
+    });
+
+
+    $(window).scroll(function () {
+
+        var top_of_element = $(".billede3").offset().top;
+        var bottom_of_element = $(".billede3").offset().top + $(".billede3").outerHeight();
+        var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+        var top_of_screen = $(window).scrollTop();
+
+        if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
+            // the element is visible, do something
+            console.log('I am visible now');
+            $('.billede3').addClass('brugAnimation');
+            //kører animationen fra css
+        } else {
+            // the element is not visible, do something else
+            console.log('I am invisible now ... fading down.')
+            $('.billede3').css('filter', 'opacity(1)');
+            // ... hide again ...
+        }
+    });
+
+
+    $(window).scroll(function () {
+
+        var top_of_element = $(".billede4").offset().top;
+        var bottom_of_element = $(".billede4").offset().top + $(".billede4").outerHeight();
+        var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+        var top_of_screen = $(window).scrollTop();
+
+        if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
+            // the element is visible, do something
+            console.log('I am visible now');
+            $('.billede4').addClass('brugAnimation');
+            //kører animationen fra css
+        } else {
+            // the element is not visible, do something else
+            console.log('I am invisible now ... fading down.')
+            $('.billede4').css('filter', 'opacity(1)');
+            // ... hide again ...
+        }
+    });
     
+    $(window).scroll(function () {
+
+        var top_of_element = $("#del1").offset().top;
+        var bottom_of_element = $("#del1").offset().top + $("#del1").outerHeight();
+        var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+        var top_of_screen = $(window).scrollTop();
+
+        if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
+            // the element is visible, do something
+            console.log('I am visible now');
+            $('#del1').addClass('brugAnimation');
+            //kører animationen fra css
+        } else {
+            // the element is not visible, do something else
+            console.log('I am invisible now ... fading down.')
+            $('#del1').css('filter', 'opacity(1)');
+            // ... hide again ...
+        }
+    });
+        
+
 }); // slut jquery
 
 
-               
-    
+
+
 /* map til siden OM */
-    mapboxgl.accessToken = 'pk.eyJ1IjoicGF1bGluZXNhIiwiYSI6ImNrZnFrZW94cjE1NjYyem9lYXg5dm5xbDEifQ.q_A2UiOFc2bEjL8-yUUwVg';
-    var map = new mapboxgl.Map({
+mapboxgl.accessToken = 'pk.eyJ1IjoicGF1bGluZXNhIiwiYSI6ImNrZnFrZW94cjE1NjYyem9lYXg5dm5xbDEifQ.q_A2UiOFc2bEjL8-yUUwVg';
+
+var map = new mapboxgl.Map({
     container: 'map', // container id
     style: 'mapbox://styles/paulinesa/cki8roynv0l2319uupwq3ciyl', // style URL
     center: [10.1876355, 56.1587063], // starting position [lng, lat]
-    zoom: 14, // starting zoom 
+    zoom: 14, // starting zoom
 });
 
-  mapboxgl.accessToken = 'pk.eyJ1IjoicGF1bGluZXNhIiwiYSI6ImNrZnFrZW94cjE1NjYyem9lYXg5dm5xbDEifQ.q_A2UiOFc2bEjL8-yUUwVg';
-    var map = new mapboxgl.Map({
-    container: 'kontaktmap', // container id
-    style: 'mapbox://styles/paulinesa/ckiio7d3j2vva19s1y8qz765s', // style URL
-    center: [10.188525, 56.158213], // starting position [lng, lat]
-    zoom: 14, // starting zoom 
-});
-                           
- 
-                          
+// tilføjer markør 
+var marker = new mapboxgl.Marker()
+.setLngLat([10.18804, 56.15867])
+.addTo(map);
+  
+
